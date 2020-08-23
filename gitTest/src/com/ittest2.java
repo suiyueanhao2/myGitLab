c0d5bf36229bad044ffa61afc1b9c8fdd0ff1f73
study <wang@163.com>
2020-08-23 22:06:46 +0800

    ceshi2

---
+++ gitTest/src/com/ittest2.java
@@ -0,0 +1,15 @@
+package com;
+
+public class ittest2 {
+    public static void main(String[] args) {
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+    }
+}
