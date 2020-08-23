a8502b83c91d5064f07eb4615a598af632283c5b
study <wang@163.com>
2020-08-23 22:08:30 +0800

    ceshi2

---
+++ gitTest/src/com/ittest3.java
@@ -0,0 +1,12 @@
+package com;
+
+public class ittest3 {
+    public static void main(String[] args) {
+        System.out.println("3333333333");
+        System.out.println("3333333333");
+        System.out.println("3333333333");
+        System.out.println("3333333333");
+        System.out.println("3333333333");
+        System.out.println("3333333333");
+    }
+}
